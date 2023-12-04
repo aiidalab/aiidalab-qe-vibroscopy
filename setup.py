e41fa41e80cf5868263dc8ad43f4822e7b33dc82
mikibonacci <mikibonacci@hotmail.it>
2023-12-04 12:53:23 +0000

    Automatic installation of phonopy via pp_install script. This works also in editable mode

--- setup.py
+++ setup.py
@@ -1,48 +1,12 @@
 import pathlib
 from setuptools import setup, find_packages
-from aiida.common.exceptions import NotExistent
-from subprocess import run
-from aiida.orm import load_code
-from aiida import load_profile
-from setuptools.command.install import install
+
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
-
-
-class CustomInstallCommand(install):
-    def run(self):
-        load_profile()
-        try:
-            load_code("phonopy@localhost")
-        except NotExistent:
-            run(
-                [
-                    "verdi",
-                    "code",
-                    "create",
-                    "core.code.installed",
-                    "--non-interactive",
-                    "--label",
-                    "phonopy",
-                    "--description",
-                    "phonopy setup by AiiDAlab.",
-                    "--default-calc-job-plugin",
-                    "phonopy.phonopy",
-                    "--computer",
-                    "localhost",
-                    "--filepath-executable",
-                    "/opt/conda/bin/phonopy",
-                ],
-                check=True,
-                capture_output=True,
-            )
-        else:
-            raise RuntimeError(f"Code phonopy is already set up!")
-        super().run()
 
 
 setup(
@@ -74,9 +38,11 @@ setup(
         "phonopy",
         "pre-commit",
     ],
-    cmdclass={
-        "install": CustomInstallCommand,
-    },
     package_data={},
     python_requires=">=3.6",
 )
+
+# Run post_install.py after installation
+import subprocess
+
+subprocess.run(["python", "pp_install.py"])
