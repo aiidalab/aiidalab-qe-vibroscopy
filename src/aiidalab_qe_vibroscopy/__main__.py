c408a15190f0c50fb2009bcedfdf279b6267398e
Miki Bonacci <miki.bonacci@psi.ch>
2024-06-29 13:29:30 +0000

    Updating to have the cli working for the phonopy code setup.

--- src/aiidalab_qe_vibroscopy/__main__.py
+++ src/aiidalab_qe_vibroscopy/__main__.py
@@ -8,7 +8,7 @@ import click
 """
 Automatic installation of the phonopy code.
 
-we suppose phonopy is already installed (pip install phonopy).
+we suppose phonopy is already installed (pip install phonopy --user).
 So we only setup in AiiDA.
 """
 
@@ -16,7 +16,9 @@ So we only setup in AiiDA.
 def cli():
     pass
 
-@cli.command()
+@cli.command(
+    help="Setup phonopy@localhost in the current AiiDA database."
+)
 def setup_phonopy():
     load_profile()
     try:
@@ -24,6 +26,11 @@ def setup_phonopy():
     except NotExistent:
         # Use shutil.which to find the path of the phonopy executable
         phonopy_path = shutil.which("phonopy")
+        if not phonopy_path:
+            raise FileNotFoundError("Phonopy code is not found in PATH.  \
+                                    You should update your PATH. If you have not phonopy in , \
+                                    your environment, install the code via \
+                                    `pip install phonopy --user`.")
         # Construct the command as a list of arguments
         command = [
             "verdi",
@@ -44,7 +51,7 @@ def setup_phonopy():
         # Use subprocess.run to run the command
         subprocess.run(command, check=True)
     else:
-        raise Warning("Code phonopy@localhost already installed!")
+        print("Code phonopy@localhost is already installed! Nothing to do here.")
 
 if __name__ == "__main__":
     cli()
